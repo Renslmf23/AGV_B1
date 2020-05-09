bc8ae07d26d5e1334898996e86a145289acfc6a5
Renslmf23 <rensvanbreukelen@gmail.com>
2020-05-09 08:27:53 +0200

    Check horizontal/vertical working

--- PythonVision/Vision.py
+++ PythonVision/Vision.py
@@ -2,7 +2,7 @@ import numpy as np
 import cv2
 
 cap = cv2.VideoCapture(0)
-lower_range = np.array([85, 50, 50])
+lower_range = np.array([85, 100, 50])
 upper_range = np.array([150, 255, 255])
 
 
@@ -18,12 +18,21 @@ def goRight():
     print("Go right")
 
 
-def findCenter(c):
-    averageX = c[0][0] + c[1][0] + c[2][0] + c[3][0]
-    averageX = averageX / 4
-    averageY = c[0][1] + c[1][1] + c[2][1] + c[3][1]
-    averageY = averageY / 4
-    return [averageX, averageY]
+def checkVertical(con):
+    longSideAngle = 0
+    if con[1][0] < con[1][1]:
+        longSideAngle = con[2] + 180
+    else:
+        longSideAngle = con[2] + 90
+    if (0 < longSideAngle < 45) or 135 < longSideAngle < 180:
+        return True
+    else:
+        return False
+
+
+def findCenter(con):
+    return con[0]
+
 
 while True:
     # Capture frame-by-frame
@@ -39,9 +48,7 @@ while True:
     contourIm = np.zeros_like(rgb)
     if len(contours) >= 2:
         guides = []
-        print("First size unsorted: ", cv2.contourArea(contours[0]))
         contours.sort(reverse=True, key=contourSize)
-        print("First size sorted: ", cv2.contourArea(contours[0]))
         for i in range(len(contours)):
             c = contours[i]
             if cv2.contourArea(c) <= 300:
@@ -51,26 +58,35 @@ while True:
                 box = cv2.boxPoints(rect)
                 box = np.int0(box)
                 cv2.drawContours(contourIm, [box], 0, (0, 0, 255), 2)
-                guides.append(box)
+                guides.append(rect)
         if len(guides) >= 2:
+            useBothGuides = bordersFound = True
             guideLeft = guides[0]
+            if checkVertical(guideLeft) is False:
+                useBothGuides = False
             guideRight = guides[1]
-            centerGuideLeft = findCenter(guideLeft)
-            centerGuideRight = findCenter(guideRight)
-            distance = abs(centerGuideLeft[0] - centerGuideRight[0])
-            width = frame.shape[1]
-            distToCenter = 0
-            if centerGuideLeft[0] < width / 2:
-                distToCenter = width / 2 - centerGuideLeft[0]
-                cv2.circle(contourIm, (int(centerGuideLeft[0] + distance / 2), int(frame.shape[0] / 2)), 8, (0, 0, 255), 2)
-            else:
-                distToCenter = width / 2 - centerGuideRight[0]
-                cv2.circle(contourIm, (int(centerGuideRight[0] + distance / 2), int(frame.shape[0] / 2)), 8, (0, 0, 255), 2)
+            if checkVertical(guideRight) is False:
+                if useBothGuides is False:
+                    bordersFound = False
+                else:
+                    useBothGuides = False
+            if bordersFound:
+                centerGuideLeft = findCenter(guideLeft)
+                centerGuideRight = findCenter(guideRight)
+                distance = abs(centerGuideLeft[0] - centerGuideRight[0])
+                width = frame.shape[1]
+                distToCenter = 0
+                if centerGuideLeft[0] < width / 2:
+                    distToCenter = width / 2 - centerGuideLeft[0]
+                    cv2.circle(contourIm, (int(centerGuideLeft[0] + distance / 2), int(frame.shape[0] / 2)), 8, (0, 0, 255), 2)
+                else:
+                    distToCenter = width / 2 - centerGuideRight[0]
+                    cv2.circle(contourIm, (int(centerGuideRight[0] + distance / 2), int(frame.shape[0] / 2)), 8, (0, 0, 255), 2)
 
-            if distToCenter > distance / 2:
-                goLeft()
-            elif distToCenter < distance / 2:
-                goRight()
+                if distToCenter > distance / 2:
+                    goLeft()
+                elif distToCenter < distance / 2:
+                    goRight()
             # Display the resulting frame
 
     cv2.line(contourIm, (int(frame.shape[1] / 2), 0), (int(frame.shape[1] / 2), frame.shape[0]), (255, 0, 0), 3)
