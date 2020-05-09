d1d7d696bf08343b127b183f77af1fa61ab16afc
Renslmf23 <rensvanbreukelen@gmail.com>
2020-05-09 20:51:34 +0200

    09-05-2020 fisheye implementation

--- PythonVision/Vision.py
+++ PythonVision/Vision.py
@@ -12,12 +12,61 @@ def contourSize(e):
     return cv2.contourArea(e)
 
 
+def rectSize(r):
+    return r[1][0] * r[1][1]
+
+
 def goLeft():
-    print("Go left")
+    # print("Go left")
+    return
 
 
 def goRight():
-    print("Go right")
+    # print("Go right")
+    return
+
+
+def stop():
+    # print("End reached")
+    return
+
+
+def singleGuide(guide):
+    centerGuide = findCenter(guide)  # find the center of the guide
+    leftGuide = centerGuide[0] < width / 2  # check whether the guide is left side or right side
+    distance = abs(centerGuide[0] - width / 2)  # get distance to center from guide
+    if leftGuide:
+        if distance > keepDistanceToLine:
+            goLeft()
+        else:
+            goRight()
+        cv2.circle(contourIm, (int(centerGuide[0] + keepDistanceToLine), int(frame.shape[0] / 2)), 8,
+                   (0, 0, 255), 2)  # draw a circle at the robots position
+    else:
+        if distance < keepDistanceToLine:
+            goLeft()
+        else:
+            goRight()
+        cv2.circle(contourIm, (int(centerGuide[0] - keepDistanceToLine), int(frame.shape[0] / 2)), 8, (0, 0, 255), 2)
+
+
+def multipleGuides(guideLeft, guideRight):
+    centerGuideLeft = findCenter(guideLeft)
+    centerGuideRight = findCenter(guideRight)
+    distance = abs(centerGuideLeft[0] - centerGuideRight[0])
+    distToCenter = 0
+    if centerGuideLeft[0] < width / 2:
+        distToCenter = width / 2 - centerGuideLeft[0]
+        cv2.circle(contourIm, (int(centerGuideLeft[0] + distance / 2), int(frame.shape[0] / 2)), 8,
+                   (0, 0, 255), 2)
+    else:
+        distToCenter = width / 2 - centerGuideRight[0]
+        cv2.circle(contourIm, (int(centerGuideRight[0] + distance / 2), int(frame.shape[0] / 2)), 8,
+                   (0, 0, 255), 2)
+    if distToCenter > distance / 2:
+        goLeft()
+    elif distToCenter < distance / 2:
+        goRight()
 
 
 def checkVertical(con):
@@ -48,6 +97,8 @@ while True:
     success, thresh = cv2.threshold(mask, 127, 255, 0)  # create threshold map from mask
     contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)  # calculate the contours
     contourIm = np.zeros_like(rgb)  # create empty mask
+    robotVision = np.zeros_like(rgb)  # create empty mask
+
     if len(contours) > 0:  # if we found contours...
         guides = []
         contours.sort(reverse=True, key=contourSize)  # sort the contours by area
@@ -62,63 +113,57 @@ while True:
                 box = np.int0(box)
                 cv2.drawContours(contourIm, [box], 0, (0, 0, 255), 2)
                 guides.append(rect)  # and add it to the guides
-        if len(guides) == 1:  # if there is only one contour that qualifies as guide, e.g. on the edges
-            guide = guides[0]
-            if checkVertical(guide) is cameraOrientation:  # check if the contour is a stop or not
-                centerGuide = findCenter(guide)  # find the center of the guide
-                width = frame.shape[1]  # get screen width
-                leftGuide = centerGuide[0] < width / 2  # check whether the guide is left side or right side
-                distance = abs(centerGuide[0] - width / 2)  # get distance to center from guide
-                if leftGuide:
-                    if distance > keepDistanceToLine:
-                        goLeft()
-                    else:
-                        goRight()
-                    cv2.circle(contourIm, (int(centerGuide[0] + keepDistanceToLine), int(frame.shape[0] / 2)), 8,
-                               (0, 0, 255), 2)  # draw a circle at the robots position
+        if len(guides) >= 1:  # multiple guides found
+            verticalGuides = []
+            horizontalGuides = []
+            width = frame.shape[1]
+            for guide in guides:
+                if checkVertical(guide):
+                    verticalGuides.append(guide)
                 else:
-                    if distance < keepDistanceToLine:
-                        goLeft()
-                    else:
-                        goRight()
-                    cv2.circle(contourIm, (int(centerGuide[0] - keepDistanceToLine), int(frame.shape[0] / 2)), 8,
-                               (0, 0, 255), 2)  # draw a circle at the robots position
-        if len(guides) >= 2:
-            useBothGuides = bordersFound = True
-            guideLeft = guides[0]
-            if checkVertical(guideLeft) is False:
-                useBothGuides = False
-            guideRight = guides[1]
-            if checkVertical(guideRight) is False:
-                if useBothGuides is False:
-                    bordersFound = False
-                else:
-                    useBothGuides = False
-            if bordersFound:
-                centerGuideLeft = findCenter(guideLeft)
-                centerGuideRight = findCenter(guideRight)
-                distance = abs(centerGuideLeft[0] - centerGuideRight[0])
-                width = frame.shape[1]
-                distToCenter = 0
-                if centerGuideLeft[0] < width / 2:
-                    distToCenter = width / 2 - centerGuideLeft[0]
-                    cv2.circle(contourIm, (int(centerGuideLeft[0] + distance / 2), int(frame.shape[0] / 2)), 8,
-                               (0, 0, 255), 2)
-                else:
-                    distToCenter = width / 2 - centerGuideRight[0]
-                    cv2.circle(contourIm, (int(centerGuideRight[0] + distance / 2), int(frame.shape[0] / 2)), 8,
-                               (0, 0, 255), 2)
+                    horizontalGuides.append(guide)
 
-                if distToCenter > distance / 2:
-                    goLeft()
-                elif distToCenter < distance / 2:
-                    goRight()
+            if len(horizontalGuides) > 0:
+                stop()
+            if len(verticalGuides) >= 2:
+                guidesLeft = []
+                guidesRight = []
+                for guide in verticalGuides:
+                    if findCenter(guide)[0] < width / 2:
+                        guidesLeft.append(guide)
+                    else:
+                        guidesRight.append(guide)
+                guidesLeft.sort(inverse=True, key=rectSize)
+                guidesRight.sort(inverse=True, key=rectSize)
+                if len(guidesLeft) > 0 and len(guidesRight) > 0:
+                    print("Both guides found")
+                    multipleGuides(guidesLeft[0], guidesRight[0])
+                    boxLeft = cv2.boxPoints(guidesLeft[0])
+                    boxLeft = np.int0(boxLeft)
+                    cv2.drawContours(robotVision, [boxLeft], 0, (0, 0, 255), 2)
+                    boxRight = cv2.boxPoints(guidesRight[0])
+                    boxRight = np.int0(boxRight)
+                    cv2.drawContours(robotVision, [boxRight], 0, (0, 0, 255), 2)
+                elif len(guidesLeft) > 0:
+                    print("Only left found")
+                    singleGuide(guidesLeft[0])
+                    boxLeft = cv2.boxPoints(guidesLeft[0])
+                    boxLeft = np.int0(boxLeft)
+                    cv2.drawContours(robotVision, [boxLeft], 0, (0, 0, 255), 2)
+                else:
+                    print("Only right found")
+                    singleGuide(guidesRight[0])
+                    boxRight = cv2.boxPoints(guidesRight[0])
+                    boxRight = np.int0(boxRight)
+                    cv2.drawContours(robotVision, [boxRight], 0, (0, 0, 255), 2)
+
             # Display the resulting frame
 
     cv2.line(contourIm, (int(frame.shape[1] / 2), 0), (int(frame.shape[1] / 2), frame.shape[0]), (255, 0, 0), 3)
     cv2.imshow('frame', rgb)
     cv2.imshow('mask', mask)
     cv2.imshow('contour', contourIm)
+    cv2.imshow('robotVision', robotVision)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
