3e3b75a64a7fd63cf3913fce5681e4c988a2d1d8
Renslmf23 <rensvanbreukelen@gmail.com>
2022-12-01 10:38:32 +0100

    Werkende websocket

--- PythonVision/PhoneRecognition/WebsocketServer.py
+++ PythonVision/PhoneRecognition/WebsocketServer.py
@@ -1,14 +1,17 @@
 import asyncio
-import datetime
-import time
-
 import websockets
+import websockets.exceptions
+import cv2
+from phone_finder import ImageHandler as IH
+from multiprocessing.pool import ThreadPool
+from collections import deque
+from get_video_frame_webcam import Camera
+import time
 
 
 class WebSocketServer:
 
-    def __init__(self, host, port, secret):
-        self._secret = secret
+    def __init__(self, host, port):
         self._server = websockets.serve(self._start, host, port)
 
     def start(self):
@@ -16,16 +19,40 @@ class WebSocketServer:
         asyncio.get_event_loop().run_forever()
 
     async def _start(self, websocket, path):
+        camera = Camera(0)
+        camera.start()
+
+        img_handler = IH("PhoneCam")
+        threads = 4
+        pool = ThreadPool(processes=threads)
+        pending = deque()
         print(f"Connected from path ={path}")
-        while True:
-            secret = await websocket.recv()
-            if secret == self._secret:
-                time.sleep(0.01)
-                msg = f"Sending message {datetime.datetime.now()}"
-                await websocket.send(msg)
-                print(msg)
+        res_img = None
+        running = True
 
+        # acquisition image: num is the image number
+        while running:
+            while len(pending) > 0 and pending[0].ready():
+                thresh, res_img, midlines = pending.popleft().get()
+                try:
+                    await websocket.send(str(time.time()))
+                except (websockets.exceptions.ConnectionClosedError, websockets.exceptions.ConnectionClosed):
+                    running = False
+                self.fit_to_screen('video', res_img)
 
-if __name__ == "__main__":
-    server = WebSocketServer("localhost", 11225, "CameraRecognizer")
-    server.start()
+            if len(pending) < threads:
+                frame = camera.frame
+                task = pool.apply_async(img_handler.find_phone, (frame.copy(),))
+                pending.append(task)
+            key = cv2.waitKey(1)
+            if key & 0xFF == ord('q'):
+                running = False
+            if key & 0xFF == ord('s'):
+                cv2.imwrite("image_{0}.png".format(time.time()), res_img)
+        camera.stop()
+        cv2.destroyAllWindows()
+        
+    def fit_to_screen(self, name, img):
+        dim = (int(img.shape[1] * 2), int(img.shape[0] * 2))
+        cv2.imshow(name, cv2.resize(img, dim, cv2.INTER_AREA))
+
--- PythonVision/PhoneRecognition/main.py
+++ PythonVision/PhoneRecognition/main.py
@@ -1,48 +1,5 @@
-import cv2
-from phone_finder import ImageHandler as IH
-from multiprocessing.pool import ThreadPool
-from collections import deque
-from get_video_frame_webcam import Camera
-from helper_functions import Line
-import time
-
-
-def fit_to_screen(name, img):
-    dim = (int(img.shape[1] * 2), int(img.shape[0] * 2))
-    cv2.imshow(name, cv2.resize(img, dim, cv2.INTER_AREA))
-
-
-def main():
-    camera = Camera(0)
-    camera.start()
-
-    img_handler = IH("PhoneCam")
-    threads = 4
-    pool = ThreadPool(processes=threads)
-    pending = deque()
-
-    time_at_last_process = 0
-    process_interval = 0.2
-    res_img = None
-    # acquisition image: num is the image number
-    while True:
-        while len(pending) > 0 and pending[0].ready():
-            thresh, res_img, midlines = pending.popleft().get()
-            fit_to_screen('video', res_img)
-
-        if len(pending) < threads:
-            frame = camera.frame
-            task = pool.apply_async(img_handler.find_phone, (frame.copy(),))
-            pending.append(task)
-        key = cv2.waitKey(1)
-        if key & 0xFF == ord('q'):
-
-            break
-        if key & 0xFF == ord('s'):
-            cv2.imwrite("image_{0}.png".format(time.time()), res_img)
-    camera.stop()
-    cv2.destroyAllWindows()
-
+from WebsocketServer import WebSocketServer
 
 if __name__ == "__main__":
-    main()
+    server = WebSocketServer("localhost", 11225)
+    server.start()
